2710cfc13799ef0db1c62db3e0ea620eedf71a58
LukeKedz <lukekedziora@gmail.com>
2021-01-15 10:28:48 -0500

    init DB setup

--- sql/card_prices_initial_01152021.sql
+++ sql/card_prices_initial_01152021.sql
@@ -1,10 +1,11 @@
--- NOTE: ran on development 1/15/21 LK
 CREATE TABLE IF NOT EXISTS card_prices (
-  sku integer not null primary key,
-  low numeric(5,2),
-  market numeric(5,2),
-  direct numeric(5,2),
-  bl_high numeric(5,2),
+  sku INTEGER not null primary key,
+
+  bl_high NUMERIC(5,2),
+  direct NUMERIC(5,2),
+  low NUMERIC(5,2),
+  market NUMERIC(5,2),
+
   created_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL
 )
---
+++ sql/db_log.md
@@ -0,0 +1,10 @@
+## Database Log
+
+### NOTES
+  0. for the time being, migrations here ran directly on DB with DBeaver
+
+### Formatting Pattern
+> [instance] [date] [file]: [explanation]
+
+### Activity
++ `development` 01152021 `card_prices_initial_01152021.sql`: base attributes established
